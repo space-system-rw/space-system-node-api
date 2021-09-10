8886384b38158e196e7350edb7fd8cec5de03f06
SamuelSabasNayo <nshusamuel1@gmail.com>
2021-09-10 20:42:56 +0200

    Added cors dependancy

--- src/database/models/review.js
+++ src/database/models/review.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Review.belongsTo(models.universityId, {
+      Review.belongsTo(models.University, {
         as: 'university',
         foreignKey: 'universityId',
         onDelete: 'CASCADE'
